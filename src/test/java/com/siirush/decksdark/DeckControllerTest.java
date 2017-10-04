7891f5e8b78ad8fcdbdeacca071a8d5cd11edc4b
Dan P. Smith <mrdanpsmith@gmail.com>
2017-10-04 00:01:34 -0700

    X out remainder of requirements.

--- src/test/java/com/siirush/decksdark/DeckControllerTest.java
+++ src/test/java/com/siirush/decksdark/DeckControllerTest.java
@@ -29,21 +29,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static net.javacrumbs.jsonunit.spring.JsonUnitResultMatchers.json;
 
 /*
-Requirements:
-·         Create a microservice that stores and shuffles card decks.
-·         A card may be represented as a simple string such as “5-heart”, or “K-spade”.
-·         A deck is an ordered list of 52 standard playing cards.
-·         Expose a RESTful interface that allows a user to:
-·         [x] PUT an idempotent request for the creation of a new named deck.  New decks are created in some initial sorted order.
-·         POST a request to shuffle an existing named deck.
-·         [x] GET a list of the current decks persisted in the service.
-·         [x] GET a named deck in its current sorted/shuffled order.
-·         [x] DELETE a named deck.
-·         [x] Design your own data and API structure(s) for the deck.
-·         [x] Persist the decks in-memory only, but stub the persistence layer such that it can be later upgraded to a durable datastore.
-·         Implement a simple shuffling algorithm that simply randomizes the deck in-place.
-·         Implement a more complex algorithm that simulates hand-shuffling, i.e. splitting the deck in half and interleaving the two halves, repeating the process multiple times.
-·         Allow switching the algorithms at deploy-time only via configuration.
+[x] Create a microservice that stores and shuffles card decks.
+[x] A card may be represented as a simple string such as “5-heart”, or “K-spade”.
+[x] A deck is an ordered list of 52 standard playing cards.
+[x] Expose a RESTful interface that allows a user to:
+[x] PUT an idempotent request for the creation of a new named deck.  New decks are created in some initial sorted order.
+[x] POST a request to shuffle an existing named deck.
+[x] GET a list of the current decks persisted in the service.
+[x] GET a named deck in its current sorted/shuffled order.
+[x] DELETE a named deck.
+[x] Design your own data and API structure(s) for the deck.
+[x] Persist the decks in-memory only, but stub the persistence layer such that it can be later upgraded to a durable datastore.
+[x] Implement a simple shuffling algorithm that simply randomizes the deck in-place.
+[x] Implement a more complex algorithm that simulates hand-shuffling, i.e. splitting the deck in half and interleaving the two halves, repeating the process multiple times.
+[x] Allow switching the algorithms at deploy-time only via configuration.
 */
 
 @RunWith(SpringRunner.class)
